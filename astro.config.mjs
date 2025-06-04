d0edca3a72ec9e4b1c3df2728fd8f33f019c4ef3
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-06-04 16:03:24 +0700

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://corsfix.com/cors-headers",
   vite: {
     plugins: [tailwindcss()],
   },
+
   base: "/cors-headers",
   trailingSlash: "never",
+
   build: {
     format: "file",
   },
+
+  integrations: [sitemap()],
 });
